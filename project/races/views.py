eaf1ab67240b4bdc7552432c646eb056c414f2a0
Joel Smith <smithjoelp@gmail.com>
2018-10-28 22:49:26 -0700

    Fleshed out the index view with race names and dates

--- project/races/views.py
+++ project/races/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import get_object_or_404, render
 from .models import Race
 
 def index(request):
-    latest_races_list = Race.objects.order_by("-created_at")[:5]
+    # latest_races_list = Race.objects.order_by("-starts")
+    latest_races_list = Race.objects.filter(is_classic=1).order_by("starts")
     context = {"latest_races_list": latest_races_list}
     return render(request, "races/index.html", context)
 
